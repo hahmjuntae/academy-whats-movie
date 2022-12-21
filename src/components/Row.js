963810901c1a64c6fc6854bd19e39176df233693
JunTaeHahm <jth5287@icloud.com>
2022-12-21 13:04:17 +0900

    Docs(*): Add all files

---
+++ src/components/Row.js
@@ -0,0 +1,98 @@
+/* Row.js */
+import React, { useEffect, useState } from 'react';
+import { A11y, Navigation, Pagination, Scrollbar } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import axios from '../api/axios';
+import '../styles/Row.css';
+import MovieModal from './MovieModal';
+
+function Row({ title, id, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelected] = useState({});
+
+  useEffect(() => {
+    fetchMovieData();
+  }, [fetchUrl]);
+
+  const fetchMovieData = async () => {
+    const request = await axios.get(fetchUrl);
+    // console.log(request);
+    setMovies(request.data.results);
+  };
+
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelected(movie);
+  };
+
+  return (
+    <section className='row'>
+      <h2>{title}</h2>
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        loop={true} // loop 기능 사용 여부
+        breakpoints={{
+          1378: {
+            slidesPerView: 6, // 한 번에 보이는 슬라이드 개수
+            slidesPerGroup: 6, // 몇개씩 슬라이드 할지
+          },
+          998: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+        }}
+        navigation // arrow 버튼 사용 유무
+        pagination={{ clickable: true }} // 페이지 버튼 보이게 할지
+      >
+        {/* <div className='slider'> */}
+        {/* <div
+            className='slider__arrow left'
+            onClick={() => {
+              document.getElementById(id).scrollLeft += window.innerWidth - 80;
+            }}>
+            <span className='arrow'>{'<'}</span>
+          </div> */}
+        <div id={id} className='row__posters'>
+          {movies.map((movie) => (
+            <SwiperSlide>
+              <img
+                src={`https://image.tmdb.org/t/p/original/${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+                key={movie.id}
+                loading=''
+                alt={movie.title || movie.name || movie.original_name}
+                onClick={() => handleClick(movie)}
+              />
+            </SwiperSlide>
+          ))}
+        </div>
+        {/* <div
+            className='slider__arrow right'
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+            }}>
+            <span className='arrow'>{'>'}</span>
+          </div> */}
+        {/* </div> */}
+      </Swiper>
+      {modalOpen && <MovieModal {...movieSelected} setModalOpen={setModalOpen} />}
+    </section>
+  );
+}
+
+export default Row;
---
+++ src/routes/SearchPage.js
@@ -0,0 +1,92 @@
+/* SearchPage.js */
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import axios from '../api/axios';
+import { useDebounce } from '../hooks/useDebounce';
+import '../styles/SearchPage.css';
+
+function SearchPage() {
+  const [searchResults, setSearchResults] = useState([]);
+  const navigate = useNavigate();
+
+  /* 현재 URL에서 정보 가져오기 */
+  // console.log(useLocation());
+
+  const useQuery = () => {
+    /* location의 search속성에서 paramas값 가져오기 */
+    return new URLSearchParams(useLocation().search);
+  };
+
+  /* params중에서 q속성만 가져오기 */
+  let query = useQuery();
+  const searchTerm = query.get('q');
+
+  /* useDebounce hook함수를 거쳐 0.5초동안 searchTerm의 새로운 값이 없다면 새로 변수에 할당 */
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+  /* q속성이 있을 경우에만 실행 */
+  useEffect(() => {
+    if (debouncedSearchTerm) {
+      fetchSearchMovie(debouncedSearchTerm);
+    }
+  }, [debouncedSearchTerm]); // q속성이 변경 될 때마다 재렌더링
+
+  const fetchSearchMovie = async (searchTerm) => {
+    try {
+      const request = await axios.get(`/search/multi?include_adult=false&query=${searchTerm}`);
+      console.log(request);
+      setSearchResults(request.data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log(searchResults);
+
+  const renderSearchResults = () => {
+    return searchResults.length > 0 ? ( // 검색어가 있는지 삼항연산자 사용
+      debouncedSearchTerm ? (
+        <section className='search-container'>
+          {searchResults.map((movie) => {
+            if (movie.backdrop_path !== null && movie.media_type !== 'person') {
+              const movieImageUrl = 'https://image.tmdb.org/t/p/w400' + movie?.backdrop_path;
+              return (
+                <div className='movie' key={movie.id}>
+                  <div
+                    className='movie__column-poster'
+                    onClick={() => {
+                      navigate(`/${movie.id}`);
+                    }}>
+                    <img
+                      src={movieImageUrl}
+                      className='movie__poster'
+                      alt={movie.title || movie.name || movie.original_name}
+                    />
+                    <div className='poster__detail'>
+                      <h3 className='poster__detail-title'>{movie.title}</h3>
+                      <p className='poster__detail-date'>{movie.release_date}</p>
+                    </div>
+                  </div>
+                </div>
+              );
+            }
+          })}
+        </section>
+      ) : (
+        navigate('/')
+      )
+    ) : debouncedSearchTerm ? (
+      <section className='no-results'>
+        <div className='no-results__text'>
+          <p>검색어 "{debouncedSearchTerm}" 에 맞는 결과가 없습니다.</p>
+        </div>
+      </section>
+    ) : (
+      navigate('/')
+    );
+  };
+
+  return renderSearchResults();
+}
+
+export default SearchPage;
